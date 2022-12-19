05ae36e22a6316a65fd6842ecacec72bfce67662
N37R09U3 <n37r09u3@gmail.com>
2022-12-20 01:05:02 +0800

    新的路由映射测试

--- routers/router.go
+++ routers/router.go
@@ -6,5 +6,6 @@ import (
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/hi", &controllers.MainController{}, "get:Hi")
 }
